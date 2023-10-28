15ff103979485ba2bf70364f414b3484b83bfa11
Oktavian Gniot <tavin@tavin.de>
2023-10-28 04:25:11 +0200

    added connection recognition, and correct address for sending packets
    Signed-off-by: Oktavian Gniot <tavin@tavin.de>

--- Robot/PicoDevice/PicoDevice.py
+++ Robot/PicoDevice/PicoDevice.py
@@ -5,17 +5,26 @@ class PicoDevice(RobotDevice):
 
     def __init__(self, component_config):
         super().__init__(component_config)
+        self._connected_to_maiun_hub = False
        # self.build()
+
+    def is_connected(self):
+        return(self._connected_to_maiun_hub)
 
     def send_data(self, data: RemoteData) -> bool:
         # print("ARD: send Data", data_packet)
         #data.set_source_address(self.get_id())
-        data.set_source_address(22)
-        data.set_destination_address(55)
+        data.set_source_address(self._id)
+        data.set_destination_address(0)
         print("send : ",str(data))
         if self._transmitter is None:
             return False
         return self._transmitter.transmitt(data)
 
-
+    def process_ping_response(self,  Msg_pingResponse):
+        print("got ping22")
+        
+        if Msg_pingResponse.get_source_address() == 0:
+            self._connected_to_maiun_hub = True
+            print("connected_to_maiun_hub")
   
