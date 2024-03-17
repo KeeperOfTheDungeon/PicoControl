927cb6ab87ade0e1952d10a2dca45175e97eabbc
Patrick Braun <me+git@pb.ax>
2024-03-17 05:15:00 +0100

    Fix main_hub typo

--- Robot/PicoDevice/PicoDevice.py
+++ Robot/PicoDevice/PicoDevice.py
@@ -1,30 +1,29 @@
 from RoboControl.Com import RemoteData
 from RoboControl.Robot.AbstractRobot.AbstractRobotDevice import AbstractRobotDevice
 from RoboControl.Robot.Device.RobotDevice import RobotDevice
+
+
 class PicoDevice(RobotDevice):
 
     def __init__(self, device_meta_data):
         super().__init__(device_meta_data)
-        self._connected_to_maiun_hub = False
-       # self.build()
+        self._connected_to_main_hub = False
 
     def is_connected(self):
-        return(self._connected_to_maiun_hub)
+        return(self._connected_to_main_hub)
 
     def send_data(self, data: RemoteData) -> bool:
-        # print("ARD: send Data", data_packet)
-        #data.set_source_address(self.get_id())
         data.set_source_address(self._id)
         data.set_destination_address(0)
-        print("send : ",str(data))
+        print("send : ", str(data))
         if self._transmitter is None:
             return False
         return self._transmitter.transmitt(data)
 
-    def process_ping_response(self,  Msg_pingResponse):
-        print("got ping22")
+    def process_ping_response(self, Msg_pingResponse):
+        print("Received ping")
         
         if Msg_pingResponse.get_source_address() == 0:
-            self._connected_to_maiun_hub = True
-            print("connected_to_maiun_hub")
+            self._connected_to_main_hub = True
+            print("connected_to_main_hub")
   
