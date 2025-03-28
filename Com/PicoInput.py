1a31505f0560482b249fe6c2204ffa33c5a5d779
Lukas Cebatavicius <lukasceb@outlook.com>
2025-03-28 20:07:34 +0100

    try to get connection

--- Com/PicoInput.py
+++ Com/PicoInput.py
@@ -10,12 +10,8 @@ class PicoInput(RemoteDataInput):
         Pin(rxpin, Pin.IN, Pin.PULL_UP)
         rx = rx_factory(clock_pin)
         self._state_machine_rx = rp2.StateMachine(connection_counter, rx, freq=10000000, in_base=Pin(rxpin), jmp_pin=Pin(rxpin))
-
         self._state_machine_rx.irq(self.interrupt_callback)
-
-        
         self._data_packet = DataPacketPico()
-        
         self._state_machine_rx.active(1)
         self.running = True
 
@@ -91,4 +87,106 @@ def rx_factory(clock_pin):
         push()				# push data to RX FIFO
 
         wrap()
+    return rx
+
+class UARTInput(RemoteDataInput):
+    def __init__(self, connection_counter, rxpin, baudrate=115200):
+        self.led_onboard = Pin(5, Pin.OUT)
+        rx_pin = Pin(rxpin, Pin.IN, Pin.PULL_UP)
+        
+        rx = uart_rx_factory(baudrate)
+        self._state_machine_rx = rp2.StateMachine(
+            connection_counter, 
+            rx, 
+            freq=10000000,
+            in_base=rx_pin, 
+            jmp_pin=rx_pin
+        )
+
+        self._state_machine_rx.irq(self.interrupt_callback)
+        self._data_packet = DataPacketPico()
+        self._state_machine_rx.active(1)
+        self.running = True
+
+    def process(self):
+        while self._state_machine_rx.rx_fifo() > 0:
+                
+            token = self._state_machine_rx.get()
+            print("Received byte:", token) 
+            if self._data_packet.putToken(token) == DataPacketPico.PACKET_READY:
+                try:
+                    remote_data = self._data_packet.decode()
+                except MemoryError:
+                    # Out of memory, dropping packet
+                    print("Out of memory, dropped an incoming packet")
+                    self._data_packet = DataPacketPico() # clear packet
+                    continue
+                
+                data_packet = remote_data.get_data_packet()
+                self.deliver_packet(data_packet)
+
+    def stop(self):
+        self.running = False
+        self._state_machine_rx.active(0)
+
+    def sync_error(self):
+        print('sync error: usart did not recieve end bit')
+        #ToDo abfangen und rücksetzen
+        
+
+    def interrupt_callback(self, x):
+        print('Error in state machine. Interrupt thrown')
+
+def uart_rx_factory(baudrate=115200):
+    bit_time = int(10000000 / baudrate)
+
+    @rp2.asm_pio(in_shiftdir=rp2.PIO.SHIFT_RIGHT,
+                fifo_join=rp2.PIO.JOIN_RX)
+    def rx():
+        wrap_target()
+        
+        label("start_bit_wait")
+        jmp(pin, "start_bit_wait")  # If pin is high, keep waiting
+        
+        # delay
+        set(y, bit_time // 2)  # Half bit time
+        label("start_bit_delay")
+        jmp(y_dec, "start_bit_delay") [1]
+        
+        set(x, 8) # 8 bits
+        
+        label("bit_loop")
+        
+        # delay to read at middle
+        set(y, bit_time)
+        label("bit_delay")
+        jmp(y_dec, "bit_delay") [1]
+        
+        
+        in_(pins, 1) # sample
+        
+        jmp(x_dec, "bit_loop")
+        
+        # check stop
+        set(y, bit_time)
+        label("stop_bit_delay")
+        jmp(y_dec, "stop_bit_delay") [1]
+        
+        # framing error
+        jmp(pin, "valid_stop_bit")
+        
+        # Handle framing error
+        set(y, 1)
+        in_(y, 23)  # Shift into MSB of the output word
+        jmp("finish")
+        
+        # everything okay
+        label("valid_stop_bit")
+        in_(null, 23)  # Add a 0 in error bit
+        
+        label("finish")
+        push()
+        
+        wrap()
+    
     return rx
---
+++ Robot/PicoDevice/PicoDeviceDataHub.py
@@ -0,0 +1,28 @@
+from RoboControl.Robot.Device.RobotDevice import RobotDevice
+
+
+class PicoDeviceDataHub(RobotDevice):
+
+    def __init__(self, device_meta_data):
+        super().__init__(device_meta_data)
+        self.channels = []
+
+        
+    def is_connected(self):
+        return(self._connected_to_main_hub)
+
+    def send_data(self, data: RemoteData) -> bool:
+        data.set_source_address(self._id)
+        data.set_destination_address(0)
+        print("send : ", str(data))
+        if self._transmitter is None:
+            return False
+        return self._transmitter.transmitt(data)
+
+    def process_ping_response(self, Msg_pingResponse):
+        print("Received ping")
+        
+        if Msg_pingResponse.get_source_address() == 0:
+            self._connected_to_main_hub = True
+            print("connected_to_main_hub")
+  
